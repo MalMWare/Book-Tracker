8b1a7f2a81bd00e16147a74999ef9cd1dcb1e09b
MalMWare <malkelleymartin@gmail.com>
2023-09-03 22:30:46 -0700

    data appears in graphiql

--- index/schema/schema.js
+++ index/schema/schema.js
@@ -1,16 +1,16 @@
 // import { graphqlHTTP } from 'express-graphql';
 const  graphql  = require('graphql');
 const _ = require('lodash')
+//import { graphqlLodash } from 'graphql-lodash';
 global._ = _
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 //Dummy data
 let books = [
-    { name: 'Attack on Titan Vol. 01', genre: 'Horror', Id: '1'},
-    { name: 'Lore Olympus', genre: 'Fantasy', Id: '2'},
-    { name: 'The Song of Achilles', genre: 'Historical Fiction', Id: '3'},
+    { name: 'Attack on Titan Vol. 01', genre: 'Horror', id: '1'},
+    { name: 'Lore Olympus', genre: 'Fantasy', id: '2'},
+    { name: 'The Song of Achilles', genre: 'Historical Fiction', id: '3'},
 ]
-
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -29,7 +29,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString }},
             resolve(parent, args){
                 //code to get data from database/other source
-                return_.find(books,{ id: args.id });
+                return _.find(books,{ id: args.id })
             }
         }
     }
