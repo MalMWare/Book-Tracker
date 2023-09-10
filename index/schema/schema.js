ec950fdfbf44cbc4d6e61ff1d76d8d8ea3937212
MalMWare <malkelleymartin@gmail.com>
2023-09-09 23:06:53 -0700

    added more data

--- index/schema/schema.js
+++ index/schema/schema.js
@@ -14,12 +14,13 @@ const {
 
 //Dummy data
 let books = [
-    { name: 'Attack on Titan Vol. 01', genre: 'Horror', id: '1', authorId: '1' },
-    { name: 'Lore Olympus', genre: 'Fantasy', id: '2', authorId: '2' },
-    { name: 'The Song of Achilles', genre: 'Historical Fiction', id: '3', authorId: '3' },
-    { name: 'Attack on Titan Vol. 02', genre: 'Horror', id: '4', authorId: '1' },
-    { name: 'Attack on Titan Vol. 03', genre: 'Horror', id: '5', authorId: '1' },
-    { name: 'Circe', genre: 'Fantasy', id: '6', authorId: '3' }
+    { name: 'Attack on Titan Vol. 01', genre: 'Horror', pages: '193', published: 'March 17, 2010', id: '1', authorId: '1' },
+    { name: 'Lore Olympus Vol. 01', genre: 'Fantasy', pages: '384', published: 'November 2, 2021', id: '2', authorId: '2' },
+    { name: 'The Song of Achilles', genre: 'Historical Fiction', pages: '408', published: 'September 20, 2011', id: '3', authorId: '3' },
+    { name: 'Attack on Titan Vol. 02', genre: 'Horror', pages: '189', published: 'July 17, 2010', id: '4', authorId: '1' },
+    { name: 'Attack on Titan Vol. 03', genre: 'Horror', pages: '208', published: 'December 9, 2010', id: '5', authorId: '1' },
+    { name: 'Circe', genre: 'Fantasy', pages: '393', published: 'April 10, 2018', id: '6', authorId: '3' },
+    { name: 'Lore Olympus Vol. 02', genre: 'Fantasy', pages: '368', published: 'July 5, 2022', id: '7', authorId: '2' }
 ]
 
 let authors = [
@@ -34,6 +35,8 @@ const BookType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        pages: { type: GraphQLString },
+        published: { type: GraphQLString },
         author: {
             type: AuthorType,
             resolve(parent, args){
